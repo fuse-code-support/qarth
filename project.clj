9680183ddc3020a4c3bc14515d1f4932f1741eca
Pavan Mantha <pavan.mantha@quintype.com>
2019-09-24 22:30:19 +0530

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject qarth "0.2.0"
+(defproject qarth "0.2.1"
   :description "OAuth for serious people"
   :url "https://github.com/mthvedt/qarth"
   :license {:name "Eclipse Public License"
